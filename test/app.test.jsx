900a1b0208a9d0a73e32a53da5cc1af3d9a9d170
Akinremi Olumide <akinremiolumide@MacBook-Pro-4.local>
2019-03-22 08:54:31 +0100

    chore(test): setup travis and coveralls

---
+++ test/app.test.jsx
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/app';
+
+it('renders without crashing', () => {
+  ReactDOM.render(<App />, document.createElement('div'));
+});
