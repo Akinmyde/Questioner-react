30260e19d535929ff5da1865dba81e230c1451e8
Akinremi Olumide <akinremiolumide@MacBook-Pro-4.local>
2019-03-29 19:09:00 +0100

    Feat(index): implement app homePage
    - delete helpers folder
    - install style-loader
    - install file-loader
    - setup css with webpack
    
    [finishies #164983815]

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
+import HomePage from './components/HomePage';
 
 class App extends Component {
   constructor(props) {
@@ -7,9 +12,14 @@ class App extends Component {
   }
 
   render() {
-    const msg = 'welcome to Questioner App';
     return (
-      <div>{msg}</div>
+      <div>
+        <Router>
+          <Header />
+          <Route path="/" component={HomePage} />
+        </Router>
+        <Footer />
+      </div>
     );
   }
 }
--- test/app.test.jsx
+++ test/app.test.jsx
@@ -1,7 +1,7 @@
 /* eslint-env mocha */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/app';
+import App from '../src/App';
 
 it('renders without crashing', () => {
   ReactDOM.render(<App />, document.createElement('div'));
