fc3cc60ea66cd4da5106d55a17fafed826d2ee2e
Akinremi Olumide <akinremiolumide96@gmail.com>
2019-04-05 19:01:29 +0100

    164983815-feature(index): implement index page

--- __test__/app.test.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-env mocha */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../src/App';
-
-it('renders without crashing', () => {
-  ReactDOM.render(<App />, document.createElement('div'));
-});
