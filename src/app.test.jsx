25d8a3402158bb830a5c04fa3b995685ecf3219f
Akinremi Olumide <akinremiolumide96@gmail.com>
2019-04-12 13:11:35 +0100

    165156028-feature(fix): fix failing test

--- src/app.test.jsx
+++ src/app.test.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-
-describe('Landing Page', () => {
-  it('should render app correctly', () => {
-    const app = shallow(<App />);
-    expect(app).toMatchSnapshot();
-  });
-});
+import App from './App';
 
 describe('Landing Page', () => {
   it('should render app correctly', () => {
