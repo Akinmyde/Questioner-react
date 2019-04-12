bebff470672ffe7a91934cd29d5062ecf082dd87
Akinremi Olumide <akinremiolumide96@gmail.com>
2019-04-12 14:48:01 +0100

    feature/165156028-ft-auth-165156028

--- src/app.test.jsx
+++ src/app.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// eslint-disable-next-line import/extensions
-import App from './App.jsx';
+import App from './App';
 
 describe('Landing Page', () => {
   it('should render app correctly', () => {
